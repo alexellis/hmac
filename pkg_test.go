919593f79f2e53140d397ac62b6daf59e94411a7
Alex Ellis <alexellis2@gmail.com>
2018-06-25 00:00:06 +0300

    Add "Sign" function
    Sign function allows HMAC package to be used to generate hashes as
    well as for verifying them.
    
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- pkg_test.go
+++ pkg_test.go
@@ -1,6 +1,9 @@
 package hmac
 
-import "testing"
+import (
+	"encoding/hex"
+	"testing"
+)
 
 func Test_GenerateInvalid_GivesError(t *testing.T) {
 
@@ -11,11 +14,27 @@ func Test_GenerateInvalid_GivesError(t *testing.T) {
 	if err == nil {
 		t.Errorf("expected error when signature didn't have at least 5 characters in length")
 		t.Fail()
+		return
 	}
 
 	wantErr := "invalid xHubSignature, should have at least 5 characters"
 	if err.Error() != wantErr {
 		t.Errorf("want: %s, got: %s", wantErr, err.Error())
+		t.Fail()
+	}
+}
+
+func Test_SignWithKey(t *testing.T) {
+	digest := "sign this message"
+	key := []byte("my key")
+
+	wantHash := "6791a762f7568f945c2e1e396cea243e944100a6"
+
+	hash := Sign([]byte(digest), key)
+	encodedHash := hex.EncodeToString(hash)
+
+	if encodedHash != wantHash {
+		t.Errorf("Sign want hash: %s, got: %s", wantHash, encodedHash)
 		t.Fail()
 	}
 }
